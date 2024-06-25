636ec0c5cf20530b92e42df0a3c9d3ec51020756
sunniesfish <hhch5611.jh@gmail.com>
2024-06-25 21:07:39 +0900

    S3 업로드 구현 중

--- src/main/java/com/learnway/global/exceptions/S3Exception.java
+++ src/main/java/com/learnway/global/exceptions/S3Exception.java
@@ -1,2 +1,11 @@
-package com.learnway.global.exceptions;public class S3Exception {
+package com.learnway.global.exceptions;
+
+
+public class S3Exception extends Exception{
+
+    public S3Exception() {
+    }
+    public S3Exception(String message) {
+        super(message);
+    }
 }
