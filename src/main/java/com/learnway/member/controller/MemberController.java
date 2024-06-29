6ac3c4ecb416fa0fc060c144af8c97ff6cf3bc6c
PastelQ <a0941@naver.com>
2024-06-29 19:03:42 +0900

    2024-06-29 / 가입 인덱스 수정

--- src/main/java/com/learnway/member/controller/MemberController.java
+++ src/main/java/com/learnway/member/controller/MemberController.java
@@ -2,13 +2,14 @@ package com.learnway.member.controller;
 
 import com.learnway.member.dto.JoinDTO;
 import com.learnway.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,13 +21,35 @@ public class MemberController {
     // Get 요청 시 회원가입 폼 리턴
     @GetMapping("/join")
     public String join(Model model) {
+        model.addAttribute("joinDTO", new JoinDTO());
         return "join";
     }
 
     // Post 요청 시 회원 가입 처리
     @PostMapping("/join")
-    public String join(JoinDTO joinDTO){
+    public String join(@Valid @ModelAttribute JoinDTO joinDTO,
+                       BindingResult bindingResult,
+                       Model model){
+        // 유효성 검사 진행 후 에러 메세지 전달
+        if (bindingResult.hasErrors()) {
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                model.addAttribute(error.getField() + "Error", error.getDefaultMessage());
+            }
+            return "join";
+        }
+        // Password / ConfirmPassword 일치 여부 확인
+        if (!joinDTO.getPassword().equals(joinDTO.getConfirmPassword())) {
+            model.addAttribute("passwordError", "비밀번호가 일치하지 않습니다.");
+            return "join";
+        }
+
         memberService.joinMember(joinDTO);
-        return "redirect:/";
+        return "redirect:/member/joinSuccess"; // 회원 가입 성공 시 로그인 창
+    }
+
+    // 회원 가입 성공 페이지
+    @GetMapping("/joinSuccess")
+    public String joinSuccess() {
+        return "joinSuccess";
     }
 }
---
+++ src/main/java/com/learnway/member/domain/MemberTelecom.java
@@ -0,0 +1,11 @@
+package com.learnway.member.domain;
+
+public enum MemberTelecom {
+    SKT,
+    KT,
+    LG,
+    // 알뜰폰
+    SKT_ALT,
+    KT_ALT,
+    LG_ALT
+}
