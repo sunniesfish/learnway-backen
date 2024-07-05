ef5f4f32451273868f6dc6c5ec08d102a8584fa6
ljh0808 <zmdk200632@gmail.com>
2024-07-05 15:46:52 +0900

    dev 받기전 커밋

--- src/main/java/com/learnway/study/domain/StudyTagRepositoryImpl.java
+++ src/main/java/com/learnway/study/domain/StudyTagRepositoryImpl.java
@@ -1,16 +1,12 @@
 package com.learnway.study.domain;
 
 
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class StudyTagRepositoryImpl implements StudyTagRepositoryCustom {
@@ -18,24 +14,35 @@ public class StudyTagRepositoryImpl implements StudyTagRepositoryCustom {
     @PersistenceContext
     private  EntityManager entityManager;
 
+//    @Override
+//    public List<StudyTag> findByTag(List<String> tags) {
+//    	
+//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<StudyTag> query = cb.createQuery(StudyTag.class);
+//        Root<StudyTag> root = query.from(StudyTag.class);
+//
+//        List<Predicate> predicates = new ArrayList<>();
+//        for (String tag : tags) {
+//            predicates.add(cb.like(root.get("tag"), "%" + tag + "%"));
+//        }
+//
+//        query.select(root).where(cb.and(predicates.toArray(new Predicate[0])));
+//
+//        return entityManager.createQuery(query).getResultList();
+//    }
+    
     @Override
+    @Transactional(readOnly = true)
     public List<StudyTag> findByTag(List<String> tags) {
-    	System.out.println("--1--");
-    	
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        System.out.println("--2--");
-        CriteriaQuery<StudyTag> query = cb.createQuery(StudyTag.class);
-        System.out.println("--3--");
-        Root<StudyTag> root = query.from(StudyTag.class);
-        System.out.println("--4--");
+        // 태그들을 콤마로 연결하여 하나의 문자열로 만듭니다.
+        String tagPattern = String.join(",%", tags) + ",%";
 
-        List<Predicate> predicates = new ArrayList<>();
-        for (String tag : tags) {
-            predicates.add(cb.like(root.get("tag"), "%" + tag + "%"));
-        }
-
-        query.select(root).where(cb.and(predicates.toArray(new Predicate[0])));
-
-        return entityManager.createQuery(query).getResultList();
+        // 네이티브 SQL 쿼리 작성
+        String sql = "SELECT * FROM study_tag WHERE tag LIKE :tagPattern";
+        
+        // 쿼리 실행	
+        return entityManager.createNativeQuery(sql, StudyTag.class)
+                            .setParameter("tagPattern", "%" + tagPattern)
+                            .getResultList();
     }
 }
