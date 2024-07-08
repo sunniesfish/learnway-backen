b0619d3e33ed554ea57e2f44c653132a87c970c3
NANOOS105 <heesu105@naver.com>
2024-07-08 16:55:04 +0900

    Merge branch 'dev' of https://github.com/sunniesfish/learnway-backend.git into feature/schedule

--- src/main/java/com/learnway/notice/controller/NoticeController.java
+++ src/main/java/com/learnway/notice/controller/NoticeController.java
@@ -135,7 +135,7 @@ public class NoticeController {
 		} catch (S3Exception e) {
 			e.printStackTrace();
 		}
-		noticeService.write(dto);
+		noticeService.write(dto, member);
 		return "redirect:/notice/noticeList";
 	}
 	
